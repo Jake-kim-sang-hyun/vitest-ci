8e80637d536ba73e804845096ec933e700071805
Jake-kim-sang-hyun <147456354+Jake-kim-sang-hyun@users.noreply.github.com>
2025-03-27 17:57:27 +0900

    ㅇㅇㅇ

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,9 @@ import { loadEnv } from "vite";
 
 export default defineConfig({
   test: {
-    reporters: process.env.GITHUB_ACTIONS ? ["dot", "github-actions"] : ["dot"],
+    reporters: process.env.GITHUB_ACTIONS
+      ? ["junit", "default", "github-actions"]
+      : ["junit", "default"],
+    // reporters: process.env.GITHUB_ACTIONS ? ["dot", "github-actions"] : ["dot"],
   },
 });
